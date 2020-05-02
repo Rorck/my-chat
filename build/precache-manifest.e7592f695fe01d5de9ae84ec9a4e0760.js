81e55d53d6181f5f1aaff3a1c6b873ca6fb64988
Rorck <sz.ricsi@hotmail.com>
2020-05-02 23:34:54 +0200

    added build

---
+++ build/precache-manifest.e7592f695fe01d5de9ae84ec9a4e0760.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "682f43de8c4aa51aacc2b819a2a7096b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "807b3134f5e2d1f867e1",
+    "url": "./static/css/main.0534c001.chunk.css"
+  },
+  {
+    "revision": "5ed28e986c9d3ccfaaa3",
+    "url": "./static/js/2.0f4066fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.0f4066fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "807b3134f5e2d1f867e1",
+    "url": "./static/js/main.f7b314fa.chunk.js"
+  },
+  {
+    "revision": "3b5176513c78f0fdd135",
+    "url": "./static/js/runtime-main.03defe92.js"
+  }
+]);
